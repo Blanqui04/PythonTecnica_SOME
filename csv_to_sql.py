43131f791a7dae34f911116da533fd4253e7b6ac
Roger Parramon <rparramon@some.es>
2025-05-08 11:35:49 +0200

    Millora de la funció d'importació, nova funció per tractar el csv. Mapping entre noms del csv i la bbdd.

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,7 +1,9 @@
 import os
 import psycopg2
 import pandas as pd
+from maps_columnes import table_mappings, column_mapping
 from dades_kop_a_csv import client, ref_project, datasheets_folder
+
 
 """ Codi per importar dades dels fitxers CSV genereta a la BBDD PostgreSQL.
 """
@@ -13,30 +15,74 @@ db_params = {
     'password': 'Some2025.!$%'
 }
 
-# Mapping of tables and their corresponding columns
-table_mappings = {
-    'table1': ['columna1', 'columna2', 'columna3'], # Columns for table1
-    'table2': ['columna4', 'columna5'],             # Columns for table2
-    'table3': ['columna6', 'columna7', 'columna8'], # Columns for table3
-}
-
 # Ruta del fitxer CSV
 csv_datasheet = os.path.join(datasheets_folder, fr"dades_escandall {client} {ref_project}.csv") # Ruta del CSV (datasheet) generat
 print(f"Ruta del CSV: {csv_datasheet}")  # Imprimir la ruta del fitxer CSV generat
 
-def importar_csv_a_sql(csv, tab):
-    """
-    Funció per importar un fitxer CSV a una taula SQL.
-    """
-    df = pd.read_csv(csv) # Llegir el fitxer CSV
-    print(df.head())  # Print the first few rows of the DataFrame for debugging
-    
-    # Step 1: Clean the DataFrame
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]    # Drop unnecessary columns (example: drop columns with "Unnamed" in their name)
-    df = df.where(pd.notnull(df), None) # Replace NaN with None for SQL compatibility# Replace NaN values with NULL (PostgreSQL-compatible)
-    print("Cleaned DataFrame:")
-    print(df.head())  # Print the cleaned DataFrame for debugging
+def datasheet_dep(csv):
+    df = pd.read_csv(csv)  # Llegir el fitxer CSV
 
+    # Step 1: Clean the DataFrame
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # Elimina les columnes innecessàries
+    df = df.where(pd.notnull(df), None)  # Substitueix NaN per None per compatibilitat SQL
+
+    # Step 2: Separar la columna '2 - Mides (BANDA x PAS x GRUIX):' en 3 columnes
+    if '2 - Mides (BANDA x PAS x GRUIX):' in df.columns:
+        df[['BANDA', 'PAS', 'GRUIX']] = df['2 - Mides (BANDA x PAS x GRUIX):'].str.split('x', expand=True)
+        # Elimina espais innecessaris de les noves columnes
+        df['BANDA'] = df['BANDA'].str.strip()
+        df['PAS'] = df['PAS'].str.strip()
+        df['GRUIX'] = df['GRUIX'].str.strip()
+        # Noms de les columnes a eliminar
+        columns_delete = [r'2 - Mides (BANDA x PAS x GRUIX):', 
+                          r'Dades necessàries per estructurar una Matèria prima',
+                          r'Dades necessàries per estructurar una Matriu',
+                          r'Dades necessàries per estructurar un Component / Material Aux.',
+                          r'Dades necessàries per estructurar una peça / conjunt',
+                          r'COMPONENT ESTAMPACIÓ 1',
+                          r'COMPONENT ESTAMPACIÓ 2',
+                          r'COMPONENT 1 COMPRA',
+                          r'COMPONENT 2 COMPRA', 
+                          r'COMPONENT 3 COMPRA', 
+                          r'DADES PER ESTRUCTURAR NOVES PECES', 
+                          r'10 - Tractaments:',
+                          r'11 - Embalatge:', 
+                          r'PD. La resta d\'informació que s\'entra ja l\'aconseguèix la persona que estructura.',
+                          r'ENTRADA DADES FORMULARIS',
+                          r'7 - SOP:',
+                          r'7 - SOP:.1',
+                          r'7 - SOP:.2',] 
+        df = df.drop(columns = columns_delete, errors='ignore')  # Elimina les columnes especificades
+    
+    # Step 3: Convertir manualment les columnes de dates
+    data_columns = [
+        '10 - Data entrega matriu:', 
+        'DATA ENTREGA PLANNING MATRIU', 
+        'DATA ENTREGA LAYOUT MATRIU', 
+        'DATA FOTs MATRICER', 
+        'DATA PROBA A SOME MATRICER (PPAP)',
+        '2 - SOP projecte',
+        '3 - EOP projecte',
+        'DATA KOP',
+        'DATA ENVIAMENT PSA'
+        
+    ]  # Afegeix aquí totes les columnes que saps que són dates
+
+    for col in data_columns:
+        if col in df.columns:
+            df[col] = pd.to_datetime(df[col], errors='coerce').dt.date  # Converteix al format de només data
+
+    # Desa el DataFrame actualitzat en un nou fitxer CSV
+    output_csv = "dades_escandall_actualitzat.csv"
+    df.to_csv(output_csv, index=False)
+    print(f"DataFrame desat al fitxer: {output_csv}")
+    
+    return df  # Retorna el DataFrame actualitzat
+
+
+def importar_csv_a_sql(df, tab, col):
+    """ Funció per importar un fitxer CSV a una taula SQL.
+    """
     # Conectar a la base de dades
     try:
         connection = psycopg2.connect(
@@ -48,32 +94,40 @@ def importar_csv_a_sql(csv, tab):
         print("Conexió exitosa a la base de dades")
         cursor = connection.cursor()    
 
-        # Iterate over the table mappings
+        # Itera sobre les assignacions de taula
         for table_name, columns in tab.items():
             print(f"Inserting data into table: {table_name}")
+            print(f"Columns to insert: {columns}")
+            # Obté el mapatge de columnes per a la taula actual
+            if table_name not in col:
+                print(f"Error: No column mapping defined for table {table_name}")
+                continue
+            
+            tab_col_mapping = col[table_name]
+            renamed_df = df.rename(columns=tab_col_mapping) # Renombra les columnes del DataFrame segons el mapatge
 
-            # Filter the DataFrame for the relevant columns for the current table
-            table_df = df[columns].dropna(how='all')  # Drop rows where all columns are NaN
+            # Filtra el DataFrame per a les columnes rellevants de la taula actual
+            table_df = renamed_df[columns].dropna(how='all')  # Deixeu anar les files on totes les columnes són NaN
             print(f"Data for {table_name}:")
-            print(table_df.head())
+            print(table_df)
 
-            # Insert data into the table
+            # Insereix dades a la taula SQL
             for index, row in table_df.iterrows():
-                # Dynamically create the INSERT query
+                # Crea dinàmicament la consulta INSERT
                 placeholders = ', '.join(['%s'] * len(columns))
                 insert_query = f"""
                 INSERT INTO {table_name} ({', '.join(columns)})
                 VALUES ({placeholders});
                 """
-                # Map DataFrame columns to table columns
+                # Mapa les columnes del DataFrame a les columnes de la taula
                 values = [row[col] for col in columns]
                 cursor.execute(insert_query, values)
 
-        connection.commit()  # Commit the transaction to save changes
+        connection.commit()  # Publica l'operació per desar els canvis
         print("Dades importades amb èxit!")
         
-        cursor.close()
-        connection.close()
+        cursor.close()  # Tanca el cursor
+        connection.close()  # Tanca la connexió a la base de dades
         print("Connexió tancada.")
 
     except Exception as e:
@@ -84,7 +138,9 @@ def main():
     """
     Funció principal per executar el procés complet de tractament de dades.
     """
-    importar_csv_a_sql(csv_datasheet, table_mappings)  # Cridar la funció per importar el CSV a SQL
+    dataframe = datasheet_dep(csv_datasheet)  # Cridar la funció per netejar el CSV
+    print(f"DataFrame netejat: \n {dataframe}")
+    importar_csv_a_sql(dataframe, table_mappings, column_mapping)  # Cridar la funció per importar el CSV a SQL
 
 
 # Executar la funció principal només si el fitxer s'executa directament
