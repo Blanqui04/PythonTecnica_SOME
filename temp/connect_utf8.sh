0ace777322a4ffd3edbabb5374d32329e0b36ea1
Ernest Ceballos <u1978907@campus.udg.edu>
2025-07-28 11:16:41 +0200

    eliminar fixter temporals

--- temp/connect_utf8.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Connectant a PostgreSQL amb encoding UTF8..."
-export PGCLIENTENCODING=UTF8
-psql -h 172.26.5.159 -p 5433 -d documentacio_tecnica -U administrador --client-encoding=UTF8
